fd2af1ebbf75db5c2c49ee5e333ec290a4315aee
Diyar Faraj <diyar.faraj@gmail.com>
2019-11-04 19:59:07 +0100

    font style added

--- src/components/sidebar/Bio.js
+++ src/components/sidebar/Bio.js
@@ -1,21 +1,16 @@
-import React from "react"
-import "./sidebar.css"
+import React from 'react';
+import './sidebar.css';
 
-import diyar from "../../images/diyar.jpg"
+import diyar from '../../images/diyar-faraj.jpg';
 
 const Bio = ({ author, tagline }) => {
-  return (
-    <div className="bio-main w-75">
-      <img
-        src={diyar}
-        style={{ maxWidth: `100px` }}
-        className="profile-img"
-        alt=""
-      />
-      <h3 className="mt-2 author-bio">{author}</h3>
-      <small className="text-muted">{tagline}</small>
-    </div>
-  )
-}
+	return (
+		<div className="bio-main w-75">
+			<img src={diyar} style={{ maxWidth: `100px` }} className="profile-img" alt="" />
+			<h3 className="mt-2 author-bio">{author}</h3>
+			<small className="text-muted">{tagline}</small>
+		</div>
+	);
+};
 
-export default Bio
+export default Bio;
